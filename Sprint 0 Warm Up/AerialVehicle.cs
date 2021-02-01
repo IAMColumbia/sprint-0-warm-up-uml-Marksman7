f5d1f2ad208adf532cf4cb0f5b5a7f731e7791d9
Marksman7 <56178417+Marksman7@users.noreply.github.com>
2021-02-01 09:44:46 -0600

    I Really neet to rember that I need to commit.
    I Really neet to rember that I need to commit.

--- Sprint 0 Warm Up/AerialVehicle.cs
+++ Sprint 0 Warm Up/AerialVehicle.cs
@@ -4,7 +4,13 @@ namespace Sprint_0_Warm_Up
 {
     public abstract class AerialVehicle
     {
+        public string Name { get; set; }
+
         public int CurrentAltitude { get; set; }
+
+        public bool Isflying { get; set; }
+
+        public int MaxAltitude { get; set; }
 
         Engine Engine { get; set; }
 
@@ -13,34 +19,45 @@ namespace Sprint_0_Warm_Up
 
         }
 
-        public bool About()
-        {
-            throw new NotImplementedException();
-        }
+        public abstract string About();
+        //{
+        //throw new NotImplementedException();
+        //}
 
-        public bool TakeOff()
-        {
-            throw new NotImplementedException();
-        }
+        public abstract string TakeOff();
+        //{
+        //throw new NotImplementedException();
+        //}
 
-        public void StartEngine()
-        {
-            throw new NotImplementedException();
-        }
+        public abstract string StartEngine();
+        //{
+        //throw new NotImplementedException();
+        //}
 
-        public void FlyDown(int howMuch)
-        {
-            throw new NotImplementedException();
-        }
+        public abstract string FlyDown(int howMuch);
+        //{
+        //throw new NotImplementedException();
+        //}
 
-        internal void FlyUp()
+        internal string FlyUp()
         {
-            throw new NotImplementedException();
-        }
+            string Flys = "";
 
-        internal void FlyUp(int HowMuch)
-        {
-            throw new NotImplementedException();
-        }
+            
+
+            if ((CurrentAltitude + 1000) <= MaxAltitude)
+            {
+                CurrentAltitude = CurrentAltitude + 1000;
+            }
+
+            Flys = "This " + Name + ".AerialVehicle has a max altitude of " + MaxAltitude + "ft. \nIts current altitude is " + CurrentAltitude + " ft.\n" + Name + "." + Engine.Abouting();
+
+            return Flys;
+     }
+
+    internal abstract string FlyUp(int HowMuch);
+        //{
+            //throw new NotImplementedException();
+        //}
     }
 }
--- Sprint 0 Warm Up/Airplane.cs
+++ Sprint 0 Warm Up/Airplane.cs
@@ -8,7 +8,74 @@ namespace Sprint_0_Warm_Up
     {
         public Airplane()
         {
+            Name = "OOPFlying";
+
+            CurrentAltitude = 0;
+
+            Isflying = false;
+
+            MaxAltitude = 41000;
+
+            Engine.Istarted = false;
 
         }
+
+        public override string About()
+        {
+            string Abouts = "";
+
+                Abouts = "This " + Name + ".Airplane has a max altitude of " + MaxAltitude + "ft.\n Its currently at a altitude of " + CurrentAltitude + " ft.\n" + Name + "." + Engine.Abouting();
+
+            return Abouts;
+        }
+
+        public override string TakeOff()
+        {
+            string TakeOffs = "";
+
+                TakeOffs = Name + ".Airplane can fly its " + Engine.Abouting();
+
+            return TakeOffs;
+        }
+
+        public override string StartEngine()
+        {
+            string StartEngines = "";
+
+            Engine.Istarted = true;
+
+            StartEngines = Name + ".Airplane is flying";
+
+            return StartEngines;
+        }
+
+        public override string FlyDown(int howMuch)
+        {
+            string Flys = "";
+
+            if ((CurrentAltitude + howMuch) <= MaxAltitude && (CurrentAltitude - howMuch >= 0))
+            {
+                CurrentAltitude = CurrentAltitude - howMuch;
+            }
+
+            Flys = "This " + Name + ".AerialVehicle has a max altitude of " + MaxAltitude + "ft. \nIts current altitude is " + CurrentAltitude + " ft.\n" + Name + "." + Engine.Abouting();
+
+            return Flys;
+        }
+
+        internal override string FlyUp(int HowMuch)
+        {
+            string Flys = "";
+
+            if ((CurrentAltitude - HowMuch) <= MaxAltitude && (CurrentAltitude - HowMuch >= 0))
+            {
+                CurrentAltitude = CurrentAltitude + HowMuch;
+            }
+
+            Flys = "This " + Name + ".AerialVehicle has a max altitude of " + MaxAltitude + "ft. \nIts current altitude is " + CurrentAltitude + " ft.\n" + Name + "." + Engine.Abouting();
+
+            return Flys;
+        }
+
     }
 }
